0627596c9f360bd9bd576b52a74b5c52855b6093
vivek_nandavanam <vivek.nandavanam@turbonomic.com>
2018-04-03 12:09:54 -0400

    Changes to address: * Multiple Availability Zone * Timeout parameter added to timeout requests

--- nova/scheduler/turbonomic_scheduler.py
+++ nova/scheduler/turbonomic_scheduler.py
@@ -25,6 +25,7 @@ under the [DEFAULT] section
 scheduler_driver = nova.scheduler.turbonomic_scheduler.TurbonomicScheduler
 turbonomic_protocol = <Turbonomic_Protocol>
 turbonomic_address = <Turbonomic_Address>
+turbonomic_timeout = <Turbonomic_Timeout>
 turbonomic_username = <Turbonomic_UserName>
 turbonomic_password = <Turbonomic_Password>
 turbonomic_verify_ssl = <Verify_ssl_certificate, defaults to False>
@@ -38,11 +39,14 @@ NOTE: 1) 'scheduler_driver' might already be configured to the default scheduler
         nova boot --flavor <FLAVOR_ID> --image <IMG_UUID> --nic net-id=<NIC_ID> --availability-zone <AVAILABILITY_ZONE>:<HOST_NAME> <VM_NAME>
 
       4) In order to force NOVA deploy a new VM in an affinity group, run the following command:
-        nova boot --flavor <FLAVOR_ID> --image <IMG_UUID> --nic net-id=<NIC_ID> --hint group=<AFFINITY_GROUP_UUID> <VM_NAME>
+        nova boot --flavor <FLAVOR_ID> --image <IMG_UUID> --nic net-id=<NIC_ID> --availability-zone <AZ,e.x. nova> --hint group=<AFFINITY_GROUP_UUID> <VM_NAME>
 
       5) This script should be placed to /lib/python2.7/site-packages/nova/scheduler
 
-    At the time of writing features 3 and 4 were unavailable in OpenStack UI and could be used only from CLI
+    At the time of writing features 3 and 4 were unavailable in OpenStack UI and could be used only from CLI. Turbonomic supports CLI deployment
+    for the region whose address is equal to controller address only, e.x. Target1 address is X.X.X.10, RegionOne is on X.X.X.10, RegionTwo on
+    X.X.X.11, RegionThree on X.X.X.12 so commands 3) and 4) will succeed only for RegionOne on X.X.X.10 and will fail or timeout for RegionTwo
+    and RegionThree
 
 """
 
@@ -58,12 +62,14 @@ from nova.scheduler import scheduler_options
 
 
 import requests
+from requests import exceptions
 import json
 import uuid
 
 ext_opts = [
     cfg.StrOpt('turbonomic_protocol', default='https', help='turbonomic Server protocol, http or https'),
     cfg.StrOpt('turbonomic_address', default='URI', help='turbonomic Server address'),
+    cfg.StrOpt('turbonomic_timeout', default='60', help='turbonomic request timeout'),
     cfg.StrOpt('turbonomic_username', default='administrator', help='turbonomic Server Username'),
     cfg.StrOpt('turbonomic_password', default='administrator', help='turbonomic Server Password'),
     cfg.StrOpt('turbonomic_verify_ssl', default='False', help='Verify SSL certificate'),
@@ -80,20 +86,23 @@ class TurbonomicScheduler(driver.Scheduler):
         self.auth = (CONF.turbonomic_username, CONF.turbonomic_password)
         self.notifier = rpc.get_notifier('scheduler')
         self.j_session_id = None
+        self.region = None
+        self.turbonomic_timeout = int(CONF.turbonomic_timeout)
         self.verify_ssl = ('true' == CONF.turbonomic_verify_ssl.lower())
-        LOG.info('Initialized, URL: {}, verify_ssl: {}'.format(self.turbonomic_rest_endpoint, CONF.turbonomic_verify_ssl))
+        LOG.info('Initialized, URL: {}, verify_ssl: {}, timeout: {}'.format(self.turbonomic_rest_endpoint, self.verify_ssl,
+                                                                            self.turbonomic_timeout))
 
     def select_destinations(self, context, spec_obj):
         self.notifier.info(context, 'turbonomic_scheduler.select_destinations.start',
                            dict(request_spec=spec_obj.to_legacy_request_spec_dict()))
-        LOG.info('Selecting destinations')
+        LOG.info('Selecting destinations, CTX: {}'.format(str(context)))
 
         self.schedule = False
         self.login()
         if self.j_session_id is None:
-            raise exception.NoValidHost(reason='Error authenticating as {} / {}'.format(self.auth[0], self.auth[1]))
+            raise exception.NoValidHost(reason='Error authenticating as {}'.format(self.auth[0]))
 
-        selected_hosts = self.create_reservation(spec_obj)
+        selected_hosts = self.create_reservation(context, spec_obj)
         LOG.info('Selected hosts: {}'.format(str(selected_hosts)))
         if len(selected_hosts) == 0:
             raise exception.NoValidHost(reason='No suitable host found for flavor {}, num of VMs: {}'.format(self.flavor_name, self.vmCount))
@@ -116,28 +125,65 @@ class TurbonomicScheduler(driver.Scheduler):
         return dests
 
     def login(self):
-        LOG.info('Logging to {}'.format(self.turbonomic_rest_endpoint + 'login'))
-        auth_response = requests.post(self.turbonomic_rest_endpoint + "login", {'username': self.auth[0], 'password': self.auth[1]}, verify = self.verify_ssl)
-        if auth_response.status_code == 200:
-            self.j_session_id = auth_response.cookies['JSESSIONID']
-            LOG.info('Authenticated as {}'.format(self.auth[0]))
-        else:
-            LOG.info('Error authenticating as {}'.format(self.auth[0]))
-            raise Exception('Authentication error')
+        LOG.info('Logging in to {}'.format(self.turbonomic_rest_endpoint + 'login'))
+        try:
+            auth_response = requests.post(self.turbonomic_rest_endpoint + "login", {'username': self.auth[0], 'password': self.auth[1]},
+                                      verify = self.verify_ssl, timeout = self.turbonomic_timeout)
 
-    def get_template_uuid(self, template_name):
-        templates_response = requests.get(self.turbonomic_rest_endpoint + 'templates', cookies={'JSESSIONID': self.j_session_id}, verify = self.verify_ssl)
-        templates = json.loads(templates_response.content)
-        uuid = ''
-        for template in templates:
-            if template_name in template.get('displayName', ''):
-                uuid = template.get('uuid', '')
+            if auth_response.status_code == 200:
+                self.j_session_id = auth_response.cookies['JSESSIONID']
+                LOG.info('Authenticated as {}'.format(self.auth[0]))
+            else:
+                LOG.info('Error authenticating as {}'.format(self.auth[0]))
+                raise Exception('Authentication error')
 
-        if uuid == '':
-            raise Exception('No template found for {}'.format(template_name))
-        return uuid
+        except exceptions.ReadTimeout:
+            LOG.info('Login request timed out: {}, username: {} '.format(self.turbonomic_rest_endpoint + "login",
+                                                                                    self.auth[0]))
+            raise Exception('Login request timed out')
 
-    def create_reservation(self, spec_obj):
+    def get_dc_uuid(self, remote_addr, availability_zone):
+        LOG.info('Searching for DC: target: {}, AZ: {}'.format(remote_addr, availability_zone))
+        try:
+            entities_resp = requests.get(self.turbonomic_rest_endpoint + 'search?types=DataCenter',
+                                     cookies={'JSESSIONID': self.j_session_id}, verify=self.verify_ssl, timeout = self.turbonomic_timeout)
+
+            entities = entities_resp.json()
+            for ent in entities:
+                dc_uuid = ent.get('uuid', '')
+                dc_uuid_parts = dc_uuid.split(':')
+                if len(dc_uuid_parts) == 5 and 'OSS' == dc_uuid_parts[0] and remote_addr == dc_uuid_parts[1] \
+                        and 'DC' == dc_uuid_parts[3] and availability_zone == dc_uuid_parts[4]:
+                    self.region = dc_uuid_parts[2]
+                    LOG.info('Region: {}, DC: {}'.format(self.region, dc_uuid))
+                    return dc_uuid
+
+            raise Exception('Region not found for target {}, AZ: {}'.format(remote_addr, availability_zone))
+
+        except exceptions.ReadTimeout:
+            LOG.info('DC search request timed out: {}'.format(self.turbonomic_rest_endpoint + 'search?types=DataCenter'))
+            raise Exception('DC search request timed out')
+
+
+    def get_template_uuid(self, template_name, remote_addr):
+        full_template_name = '{}:{}::TMP-{}'.format(remote_addr, self.region, template_name)
+        try:
+            templates_response = requests.get(self.turbonomic_rest_endpoint + 'templates', cookies={'JSESSIONID': self.j_session_id},
+                                          verify = self.verify_ssl, timeout = self.turbonomic_timeout)
+
+            templates = json.loads(templates_response.content)
+            for template in templates:
+                temp_name = template.get('displayName', '')
+                if full_template_name == temp_name:
+                    return template.get('uuid', '')
+
+            raise Exception('No template found for {}'.format(full_template_name))
+
+        except exceptions.ReadTimeout:
+            LOG.info('Template request timed out: {}'.format(self.turbonomic_rest_endpoint + 'templates'))
+            raise Exception('Template request timed out')
+
+    def create_reservation(self, context, spec_obj):
         self.reservationName = "OpenStack-Placement-Request-" + str(uuid.uuid4())
         self.flavor_name = spec_obj.flavor.name
         if 'id' in spec_obj.image:
@@ -155,6 +201,7 @@ class TurbonomicScheduler(driver.Scheduler):
                 else:
                     self.scheduler_hint = ''
 
+        LOG.info('Flavor: {}, Img: {}'.format(str(spec_obj.flavor), str(spec_obj.image)))
         LOG.info('spec_obj: {}'.format(str(spec_obj)))
         selected_hosts = []
 
@@ -165,10 +212,18 @@ class TurbonomicScheduler(driver.Scheduler):
             LOG.info('force_host = {}'.format(str(selected_hosts)))
             return selected_hosts
 
-        template_uuid = self.get_template_uuid(self.flavor_name)
+        if spec_obj.availability_zone is None:
+            raise Exception('Availability zone not set')
 
-        LOG.info('Creating placement {}, Flavor: {}, DeploymentProfile: {}, SchedulerHint: {}, Template: {}' .format(
-            self.reservationName, self.flavor_name, self.deploymentProfile, str(self.scheduler_hint), template_uuid))
+        if context.remote_address is None:
+            raise Exception('Remote address not set')
+
+        dc_uuid = self.get_dc_uuid(context.remote_address, spec_obj.availability_zone)
+
+        template_uuid = self.get_template_uuid(self.flavor_name, context.remote_address)
+
+        LOG.info('Creating placement {}, DeploymentProfile: {}, SchedulerHint: {}, Template: {}, DC: {}' .format(
+            self.reservationName, self.deploymentProfile, str(self.scheduler_hint), template_uuid, dc_uuid))
 
         if self.isSchedulerHintPresent:
             constraints = ''
@@ -178,45 +233,52 @@ class TurbonomicScheduler(driver.Scheduler):
                     constraints += ','
 
             placement = '{"demandName": "' + self.reservationName + '", "action": "PLACEMENT", "parameters": [ ' \
-                '{"placementParameters": {"count": ' + str(self.vmCount) + ', "templateID": "' + template_uuid + '"},' \
+                '{"placementParameters": {"count": ' + str(self.vmCount) + ', "templateID": "' + template_uuid + '", "constraintIDs":["' + dc_uuid + '"]},' \
                 '"deploymentParameters": {"deploymentProfileID": "' + self.deploymentProfile + '", "constraintIDs":[' + constraints + ']  }}]}'
         else:
             placement = '{"demandName": "' + self.reservationName + '", "action": "PLACEMENT", "parameters": [ ' \
-                    '{"placementParameters": {"count": ' + str(self.vmCount) + ', "templateID": "' + template_uuid + '"},' \
+                    '{"placementParameters": {"count": ' + str(self.vmCount) + ', "templateID": "' + template_uuid + '", "constraintIDs":["' + dc_uuid + '"]},' \
                     '"deploymentParameters": {"deploymentProfileID": "' + self.deploymentProfile + '"}}]}'
 
         LOG.info('Placement json: {}'.format(placement))
 
-        placement_response = requests.post(self.turbonomic_rest_endpoint + 'reservations', data=placement, cookies={'JSESSIONID': self.j_session_id},
-                                           headers={'content-type': 'application/json'}, verify = self.verify_ssl)
+        try:
+            placement_response = requests.post(self.turbonomic_rest_endpoint + 'reservations', data=placement,
+                                               cookies={'JSESSIONID': self.j_session_id}, headers={'content-type': 'application/json'},
+                                               verify = self.verify_ssl, timeout = self.turbonomic_timeout)
 
-        if placement_response.status_code == 200:
-            placement = placement_response.json()
-            LOG.info('Placement created: {}'.format(str(placement)))
-            if placement['status'] == 'PLACEMENT_SUCCEEDED':
-                if 'demandEntities' in placement:
-                    demandEntity = placement['demandEntities'][0]
-                    if 'placements' in demandEntity:
-                        placements = demandEntity['placements']
-                        if 'computeResources' in placements:
-                            computeResources = placements['computeResources']
-                            for cr in computeResources:
-                                if 'provider' in cr:
-                                    provider = cr['provider']
-                                    if provider['className'] == 'PhysicalMachine':
-                                        LOG.info('Appending host: {}'.format(provider['displayName']))
-                                        selected_hosts.append(provider['displayName'])
+            if placement_response.status_code == 200:
+                placement = placement_response.json()
+                LOG.info('Placement resp: {}'.format(str(placement_response)))
+                LOG.info('Placement obj: {}'.format(str(placement)))
+                if placement['status'] == 'PLACEMENT_SUCCEEDED':
+                    if 'demandEntities' in placement:
+                        demandEntity = placement['demandEntities'][0]
+                        if 'placements' in demandEntity:
+                            placements = demandEntity['placements']
+                            if 'computeResources' in placements:
+                                computeResources = placements['computeResources']
+                                for cr in computeResources:
+                                    if 'provider' in cr:
+                                        provider = cr['provider']
+                                        if provider['className'] == 'PhysicalMachine':
+                                            LOG.info('Appending host: {}'.format(provider['displayName']))
+                                            selected_hosts.append(provider['displayName'])
+                            else:
+                                LOG.info('No compute resource found in placements')
                         else:
-                            LOG.info('No compute resource found in placements')
+                            LOG.info('No placement found')
                     else:
-                        LOG.info('No placement found')
+                        LOG.info('No demand entities found')
                 else:
-                    LOG.info('No demand entities found')
+                    LOG.info('Placement failed: {}'.format(str(placement)))
             else:
-                LOG.info('Placement failed: {}'.format(placement['status']))
-        else:
-            resp = placement_response.json()
-            LOG.info('Error creating placement: {}'.format(str(resp)))
-            raise Exception(resp['message'])
+                resp = placement_response.json()
+                LOG.info('Error creating placement: {}'.format(str(resp)))
+                raise Exception(resp['message'])
 
-        return selected_hosts
+            return selected_hosts
+
+        except exceptions.ReadTimeout:
+            LOG.info('Placement request timed out {}'.format(self.turbonomic_rest_endpoint + 'reservations'))
+            raise Exception('Placement request timed out')
